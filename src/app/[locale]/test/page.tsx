a0a36cb78c1511bc85fee15a14e5eb5d2b2ce7ed
Stani_Oni <124056306+Wyvern-2@users.noreply.github.com>
2024-11-21 16:27:33 +0400

    Done simple translation for the new page

--- src/app/[locale]/test/page.tsx
+++ src/app/[locale]/test/page.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import { useLocale, useTranslations } from 'next-intl';
 
-const NewPage: React.FC = () => {
+const TestPage = () => {
+  const locale = useLocale();
+  const t = useTranslations('TestPage'); // Подключаем переводы из TestPage
+
   return (
     <div>
-      <h1>Welcome to the New Page</h1>
-      <p>This is a new page in the project.</p>
+      <h1>{t('meta_title')}</h1>
+      <p>{t('meta_description')}</p>
     </div>
   );
 };
 
-export default NewPage;
+export default TestPage;
+
+/* const NewPage = () => {
+  // Используем i18n для загрузки переводов
+  const t = useTranslations('TestPage');
+
+  return (
+    <div>
+      <h1>{t('title')}</h1>
+      <p>{t('description')}</p>
+    </div>
+  );
+};
+
+export default NewPage; */
